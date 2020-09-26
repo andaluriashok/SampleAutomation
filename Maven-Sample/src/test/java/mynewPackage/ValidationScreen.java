ee55bd65c2b004a4ed5282b3e60e3a29b779f9d2
Ashok <ashok.mca08@gmail.com>
2020-09-26 13:33:38 +0530

    commit number 2

---
+++ Maven-Sample/src/test/java/mynewPackage/ValidationScreen.java
@@ -0,0 +1,14 @@
+package mynewPackage;
+
+public class ValidationScreen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	System.out.println("Ashok kumar");
+	
+	
+			
+	}
+
+}
