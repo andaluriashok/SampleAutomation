0b525c2ae995eba895303e99be442d90f207173e
Ashok <ashok.mca08@gmail.com>
2020-09-26 09:42:20 +0530

    code modified for test

--- Maven-Sample/src/test/java/ProjectWTG/LoginScreen.java
+++ Maven-Sample/src/test/java/ProjectWTG/LoginScreen.java
@@ -1,12 +1,21 @@
 package ProjectWTG;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class LoginScreen {
 
 @Test
-public void LaunchBrowser()
-{
-	
+public void LaunchBrowser() {
 }
+{
+
+}
+
+@AfterTest
+public void ValidatePage() {
+	System.out.println("Hello test Body added");
+}
+
+
 }
